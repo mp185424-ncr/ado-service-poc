9321c900e22b17952b53a5a0d4b716c81581a610
mp185424-ncr <mp185424@ncr.com>
2023-01-24 15:19:06 +0530

    first commit

---
+++ src/test/java/com/ncr/adoservicepoc/AdoServicePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncr.adoservicepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoServicePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
